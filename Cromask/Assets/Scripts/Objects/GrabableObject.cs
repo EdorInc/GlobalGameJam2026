c1f6aeeb6e475316e26a29fd6ccdf48ba46bad81
pzapico23 <125588835+pzapico23@users.noreply.github.com>
2026-01-31 12:51:40 +0100

    Collison changed when not grabbing

--- Cromask/Assets/Scripts/Objects/GrabableObject.cs
+++ Cromask/Assets/Scripts/Objects/GrabableObject.cs
@@ -3,17 +3,39 @@ using UnityEngine;
 public class GrabableObject : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] private LayerMask groundLayer; 
+    [SerializeField] private float groundCheckDistance = 0.6f; 
+
+
     private Transform baseParent;
     private Rigidbody Rigidbody;
-
+    
     private void Awake()
     {
         baseParent = transform.parent;
         Rigidbody = GetComponent<Rigidbody>();
         Rigidbody.freezeRotation = true;
     }
+    private void Update()
+    {
+        CheckGround();
+    }
     public Transform GetBaseParent()
     {
         return baseParent;
+    }
+    private void CheckGround()
+    {
+        Debug.DrawRay(transform.position, Vector3.down, Color.red, 5);
+
+        if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, groundCheckDistance, groundLayer))
+        {
+            Rigidbody.isKinematic = true; 
+        }
+        else
+        {
+            Rigidbody.isKinematic = false;
+        }
     }
 }
--- Cromask/Assets/Scripts/Objects/MovingObject.cs
+++ Cromask/Assets/Scripts/Objects/MovingObject.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class MovingObject : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private Transform pointA;
+    [SerializeField]
+    private Transform pointB;
 
+    private Vector3 target;
+
+    private void Start()
+    {
+        target = pointB.position;
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, target) < 0.01f)
+        {
+            target = target == pointA.position ? pointB.position : pointA.position;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Colision");
+        other.gameObject.transform.parent = transform;
     }
 }
