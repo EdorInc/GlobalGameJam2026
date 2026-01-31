0c2545b110ede0207ad7d3bf142f726c6d010707
Martín do Río Rico <mdoriorico@gmail.com>
2026-01-31 18:08:15 +0100

    Add menu assets
    Sorry they were unselected, they were meant to go with the last one.

---
+++ Cromask/Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public enum MenuState
+    {
+        Main,
+        Controls
+    }
+
+    [Header("Menu Panels")]
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject controlsMenu;
+
+    private MenuState currentState;
+
+    private void Start()
+    {
+        ShowMenu(MenuState.Main);
+    }
+
+    public void ShowMenu(MenuState newState)
+    {
+        // Disable all panels first
+        mainMenu.SetActive(false);
+        controlsMenu.SetActive(false);
+
+        // Enable the selected one
+        switch (newState)
+        {
+            case MenuState.Main:
+                mainMenu.SetActive(true);
+                break;
+
+            case MenuState.Controls:
+                controlsMenu.SetActive(true);
+                break;
+
+        }
+
+        currentState = newState;
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("RGB Scene");
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    public void OpenMainMenu()
+    {
+        ShowMenu(MenuState.Main);
+    }
+
+    public void OpenControls()
+    {
+        ShowMenu(MenuState.Controls);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
