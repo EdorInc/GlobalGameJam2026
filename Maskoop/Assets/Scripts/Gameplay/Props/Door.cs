7ecf3f66c616c712b85f9c1434472d3824880f52
pzapico23 <125588835+pzapico23@users.noreply.github.com>
2026-02-27 20:53:07 +0100

    Bridge logic done and speed added

--- Maskoop/Assets/Scripts/Gameplay/Props/Door.cs
+++ Maskoop/Assets/Scripts/Gameplay/Props/Door.cs
@@ -4,14 +4,19 @@ public class Door : MonoBehaviour
 {
     [Header("Conection Settings")]
     [SerializeField]
-    [Tooltip("Channel to use to connect to buttons. Buttons need to have the same channel to open the door")]
+    [Tooltip("Channel to use to connect to buttons. Buttons need to have the same channel to open the door/Bridge")]
     private int channel = 1;
     [SerializeField] private int buttonsNedded = 1;
 
     [Header("Position Settings")]
     [SerializeField]
-    [Tooltip("Position of the door when it opens")]
+    [Tooltip("Position of the door/bridge when it opens")]
     private Transform openPosition;
+
+    [Header("Movement Settings")]
+    [SerializeField]
+    [Tooltip("Speed to open the door/bridge")]
+    private float speed = 2;
 
     private bool shouldOpen = false;
 
@@ -28,7 +33,7 @@ public class Door : MonoBehaviour
     {
         if (shouldOpen)
         {
-            transform.position = Vector3.MoveTowards(transform.position, openPosition.position, Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, openPosition.position, Time.deltaTime * speed);
         }
         if (transform.position.Equals(openPosition.position))
         {
