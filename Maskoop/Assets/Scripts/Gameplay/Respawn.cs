bceb987bdecf4cc5d4ba3332f6eccfd39bc89d16
anaestg <ana.esteban@balanceit.org>
2026-03-02 18:11:23 +0100

    Respawn for everything

---
+++ Maskoop/Assets/Scripts/Gameplay/Respawn.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    [Header("Respawn Settings")]
+    public Vector3 respawnPosition = new Vector3(0, 1, 0);
+    public float voidDistance = -3;
+    public bool willDestroy = false;
+
+    new private Rigidbody rigidbody;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (willDestroy == false)
+        {
+            RespawnFunction();
+        }
+        else
+        {
+            Destroy();
+        }
+    }
+
+    void RespawnFunction()
+    {
+        if (rigidbody.position.y < voidDistance)
+        {
+            rigidbody.position = respawnPosition;
+        }
+    }
+
+    void Destroy()
+    {
+        Destroy(gameObject);
+    }
+}
