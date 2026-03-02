8a920ea265e76ef635a65d9d17803b8152a30833
anaestg <ana.esteban@balanceit.org>
2026-03-02 20:09:01 +0100

    Rock Spawner + Pressure Plate

---
+++ Maskoop/Assets/Scripts/Gameplay/Spawners/CubeSpawner.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] private GameObject cubePrefab;
+    [SerializeField] private Transform spawnPosition;
+
+    private GameObject cubeSpawned;
+
+    void Start()
+    {
+        SpawnCube();
+    }
+
+    private void SpawnCube()
+    {
+        if(cubeSpawned == null)
+        {
+            cubeSpawned = Instantiate(cubePrefab, spawnPosition.position, Quaternion.identity);
+        }
+        else
+        {
+            cubeSpawned.transform.position = spawnPosition.position;
+        }
+    }
+
+    public void DestroyCube(float respawnDelay = 0.0f)
+    {
+        Invoke(nameof(DestroyAndRespawnCube), respawnDelay);
+    }
+
+    private void DestroyAndRespawnCube()
+    {
+        if (cubeSpawned == null)
+            return;
+
+        Destroy(cubeSpawned);
+        cubeSpawned = null;
+
+        SpawnCube();
+    }
+}
